442690318fa82d9fbbdceabc02b2371b9c36a59a
Sebastián <palmabatun.s@gmail.com>
2017-05-23 15:24:32 +0200

    green: Creacion de array asociativo vacio

--- src/practicaTDD/AsocArray.java
+++ src/practicaTDD/AsocArray.java
@@ -2,9 +2,23 @@ package practicaTDD;
 
 public class AsocArray {
 
-	public Object size() {
-		// TODO Auto-generated method stub
-		return null;
+	private class Nodo {
+		String clave;
+		String valor;
+		Nodo siguiente;
+	}
+
+	private Nodo prim;
+	private int tam;
+	
+	public AsocArray() {
+		prim = null;
+		tam = 0;
+		
 	}
 	
+	public int size() {
+		return tam;
+	}
+
 }
--- test/practicaTDD/AsocArrayTest.java
+++ test/practicaTDD/AsocArrayTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AsocArrayTest {
+	
 	@Test
 	public void creacionDelArrayAsociativoVacio(){
 		AsocArray array = new AsocArray();
